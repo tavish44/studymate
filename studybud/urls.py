79f3f57101de6f286d356b596312ca52dc4071fc
Tavish <tavish279@gmail.com>
2024-07-10 23:09:59 +0530

    first commit

---
+++ studybud/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+#from django.http import HttpResponse
+
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('',include('base.urls')),
+    path('api/', include('base.api.urls'))
+]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
